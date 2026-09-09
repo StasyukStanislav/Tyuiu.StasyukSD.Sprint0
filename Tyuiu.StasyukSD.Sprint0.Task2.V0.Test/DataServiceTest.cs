0e4ae88705cf5db81ade119c276f4d38e90f48d6
Stanislav Stasyuk <Tyuiu.StasyukSD@outlook.com>
2026-09-09 20:21:30 +0500

    Task2

---
+++ Tyuiu.StasyukSD.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.StasyukSD.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.StasyukSD.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов тестмрования, методов из библиотеки
+            var name = "Игорь";
+            var res = DataService.GetMessage(name);
+
+            //Вызываем клвсс Asset и вызываем метод AreEqual
+            Assert.AreEqual("Привет..., Игорь", res);
+        }
+    }
+}
