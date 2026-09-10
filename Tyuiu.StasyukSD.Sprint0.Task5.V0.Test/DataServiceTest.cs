de17ef01795376b30d44acfe9373d75f7c8c23d7
Stanislav Stasyuk <Tyuiu.StasyukSD@outlook.com>
2026-09-10 21:00:35 +0500

    Task5

---
+++ Tyuiu.StasyukSD.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.StasyukSD.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.StasyukSD.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public sealed class DataServicetest
+    {
+        [TestMethod]
+        public void CheckAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+
+        [TestMethod]
+
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+
+        [TestMethod]
+
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+
+        [TestMethod]
+
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
